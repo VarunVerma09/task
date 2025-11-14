a1fffc940227cd3f1ad244bfdeae7a76926186d0
VarunVerma09 <umangsoni409@gmail.com>
2025-11-14 16:06:24 +0530

    deploymet

--- frontend/src/pages/Landing2.jsx
+++ frontend/src/pages/Landing2.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 import LeadForm from "../components/LeadForm";
 import FeesModal from "../components/FeesModal";
 import { fetchUniversity } from "../api";
